2080911a831680ef5d03f2e2abb6d6aae5b95f5b
100210110 <104227884+100210110@users.noreply.github.com>
2024-02-21 21:10:03 +0800

    modified:   "小修改，目前可用"

--- Q检测法.py
+++ Q检测法.py
@@ -16,10 +16,14 @@ while True:
         sorted(num_list)
 
         q0 = max(num_list) - min(num_list)
+        print(f"Q0 = {q0}")
         a = num_list[1] - min(num_list)
+        print(f"a = {a}")
         b = max(num_list) - num_list[-2]
+        print(f"b = {b}")
         q1 = max(a , b)
+        print(q1)
         Q1 = q1 / q0
-        print(f"Q1 = {Q1} \n请和表中数据对比")
+        print(f"Q1 = {Q1:.4g} \n请和表中数据对比\n")
     else:
         print("输入为空")
